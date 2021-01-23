3db5d725f06b03a4b31a4ea60d41f4d374da09ee
Julie Altman <jtskolnik@gmail.com>
2021-01-23 17:05:29 -0500

    deploying

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
       <NavBar />
         <Wrapper>
           <Route exact path="/" component={HomePage} />
+          <Route exact path="/skolportfolio" component={HomePage} />
           <Route exact path="/portfolio" component={Portfolio} />
         </Wrapper>
         <Footer />
