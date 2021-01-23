6ad15418189c9cb7f7bc4daa950ac6d8960f947a
Julie Altman <jtskolnik@gmail.com>
2021-01-23 11:23:44 -0500

    basic react app and dependencies set up

---
+++ src/components/Footer/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Julie Altman Portfolio</span>
+    </footer>
+  );
+}
+
+export default Footer;
+
---
+++ src/pages/HomePage.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+
+function HomePage() {
+    return (
+        <div>
+            <h1>Sample Text</h1>
+        </div>
+    )
+}
+
+export default HomePage;
